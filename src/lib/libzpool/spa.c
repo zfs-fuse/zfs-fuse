d1637d7765bec48aeadee301a5a6bc9845d9e4a2
Tim Haley <Tim.Haley@Sun.COM>
2009-11-10 15:04:47 +0100

    hg commit 10950:6897692 ZFS import leaks an nvlist

--- src/lib/libzpool/spa.c
+++ src/lib/libzpool/spa.c
@@ -2783,7 +2783,6 @@ spa_import_rootpool(char *devpath, char *devid)
 		goto out;
 	}
 
-	VERIFY(nvlist_dup(config, &spa->spa_config, 0) == 0);
 	error = 0;
 	spa_history_log_version(spa, LOG_POOL_IMPORT);
 out:
