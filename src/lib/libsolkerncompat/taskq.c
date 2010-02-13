f70518e4b2275a7fd8140fd1d65114fc95105297
Emmanuel Anne <emmanuel.anne@gmail.com>
2010-02-13 22:36:22 +0100

    fix the crash for debug builds in taskq_init
    The crash was only on some configurations, so it went totally unnoticed for
    quite some time...

--- src/lib/libsolkerncompat/taskq.c
+++ src/lib/libsolkerncompat/taskq.c
@@ -787,7 +787,9 @@ taskq_init(void)
 	    0, taskq_constructor, taskq_destructor, NULL, NULL, NULL, 0);
 	taskq_id_arena = vmem_create("taskq_id_arena",
 	    (void *)1, INT32_MAX, 1, NULL, NULL, NULL, 0,
-	    VM_SLEEP | VMC_IDENTIFIER);
+	    VM_NOSLEEP | VMC_IDENTIFIER);
+	    /* zfs-fuse : replaced VM_SLEEP by VM_NOSLEEP to pass the assertion
+	     * in umem while it's initialising... */
 
 	list_create(&taskq_cpupct_list, sizeof (taskq_cpupct_ent_t),
 	    offsetof(taskq_cpupct_ent_t, tp_link));
